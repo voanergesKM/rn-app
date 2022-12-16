a0863d8faea05167744b3866a609509400939604
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-12-16 18:49:00 +0200

    register screen

--- App.js
+++ App.js
@@ -1,25 +1,11 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  ImageBackground,
-  Dimensions,
-  TouchableOpacity,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { StyleSheet, View, ImageBackground, Platform, KeyboardAvoidingView } from 'react-native';
+import { Register } from './src/screens/RegisterScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground style={styles.image} source={require('./assets/images/mainBgImage.jpg')}>
-        <View style={styles.formContainer}>
-          <Text style={styles.formTitle}>Register</Text>
-          <View style={styles.avatar}>
-            <TouchableOpacity style={styles.addIconAvatar}>
-              <Icon name="add-circle-outline" color="#FF6C00" size={25} />
-            </TouchableOpacity>
-          </View>
-        </View>
+        <Register />
       </ImageBackground>
     </View>
   );
@@ -29,8 +15,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
 
   text: {
@@ -40,41 +24,7 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     resizeMode: 'cover',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-
-  formContainer: {
-    position: 'relative',
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 92,
-    paddingBottom: 78,
-    backgroundColor: '#FFFFFF',
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-  },
-
-  formTitle: {
-    fontSize: 30,
-    lineHeight: 35,
-    color: '#212121',
-  },
-
-  avatar: {
-    position: 'absolute',
-    top: 0,
-    backgroundColor: '#F6F6F6',
-    borderRadius: 16,
-    width: 120,
-    height: 120,
-    transform: [{ translateY: -60 }],
-  },
-
-  addIconAvatar: {
-    position: 'absolute',
-    right: -14,
-    bottom: 14,
+    // justifyContent: 'flex-end',
+    // alignItems: 'center',
   },
 });
